bff677650db11ee9e44b96c53addf73bb4dd11ef
Alexey Kosenko <alexey.kosenko@mail.ru>
2021-08-31 16:26:02 +0300

    Add JSdoc for plain function

--- source/plain.js
+++ source/plain.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/**
+ * Функция формирует массив, состоящий из элементов из всех вложенных подмассивов
+ *
+ * @param {array} array - исходный массив элементов
+ * @returns {array} - массив элементов "поднятых" из всех подмассивов
+ */
+
 const plain = array => {
     if (!Array.isArray(array)) {
         throw new TypeError('Invalid input');
